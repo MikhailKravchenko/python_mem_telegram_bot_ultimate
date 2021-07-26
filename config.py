339ab7018d442af2d45d2fd680a36ff366de87b8
MikhailKravchenko <46317577+MikhailKravchenko@users.noreply.github.com>
2021-07-27 01:50:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 
 
-database_name = '/home/lukas/testproject/pythom_mem_telegram_bot_ultimate/db.sqlite3  # Файл с базой данных
+database_name = '/home/lukas/testproject/pythom_mem_telegram_bot_ultimate/db.sqlite3'  # Файл с базой данных
 shelve_name = 'shelve.db'  #
 shelve_level = 'shelve_level.db'  #
 
